2299b5682de5e62756846ce44efa24a952ea942b
Jahill17 <104641021+Jahill17@users.noreply.github.com>
2022-07-23 16:31:41 -0400

    challenge 1 update

--- js/charts.js
+++ js/charts.js
@@ -86,7 +86,7 @@ function buildCharts(sample) {
       text: labels
     }];
       
-    // 9. Create the layout for the bar chart. 
+    // 9. Create the layout(s) for the bar chart. 
     var barLayout = {
       title: "Top 10 Bacteria Cultures Found"
     };
